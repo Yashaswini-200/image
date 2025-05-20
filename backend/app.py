88452caf5417a7a23752a19637203767f9a0eebb
yashaswini-200 <yashaswini.v2999@gamil.com>
2025-05-20 10:40:23 +0530

    changes made

--- backend/app.py
+++ backend/app.py
@@ -62,4 +62,5 @@ def handle_prediction():
     return jsonify({'error': 'No file or URL provided'}), 400
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- p.py
+++
@@ -1,9 +0,0 @@
-from backend.predictionFunction import predict_image
-if __name__ == "__main__":
-    # Manually test the backend with an image
-    image_path = "backend/training_data/ai_folder/ai_folder/7e34225424b78a952f0a3d160b.jpg"  # Replace with a real image path
-    try:
-        result = predict_image(image_path)
-        print(f"Prediction: {result}")
-    except Exception as e:
-        print(f"Error: {e}")
